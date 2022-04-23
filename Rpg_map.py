7aa84677838425e6fef4114130a277eaff8e2fb9
BaldinoMilan <104149368+BaldinoMilan@users.noreply.github.com>
2022-04-23 13:50:34 +0200

    Update Rpg_map.py

--- Rpg_map.py
+++ Rpg_map.py
@@ -1,17 +1,7 @@
 import ND_arrays as nd
-# Pour la map je vais faire un truc du style(les numéros serais l'ordre des zones):
-#[5][3][4]
-#[6][0][1]
-#[7][8][2] après ça peux changer"""
-
-# 2 idées la c'esttt on peut ranger toutes les maps dans une grande carte
-# ou séparément, ca change pas grand chose
-
-Map = nd.Nd_array((3, 3), dtype=nd.Nd_array)
-def instore_map():
-    for i in range(3):
-        for j in range(3):
-            Map[i, j] = nd.Nd_array((100, 100))
-            Map[i, j].instore((255, 0, 0))
-
-insore_map()
+block_color = {0 : (0, 255, 0)} # il n'y a que la couleur de l'herbe, je croit que c'est vert mais modifie la si c pas ça
+# c'est un dictionnaire référence de toute les couleur pour la map(si il y a des items non défini il faut les mettres la dedans)
+Map = Nd_array((901, 901))
+Map.zeros()
+middle = (450, 450)
+# modifier la map avec Map[coordonées] = truc que tu veux rajouter
