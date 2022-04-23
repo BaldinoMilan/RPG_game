8e41944e82603297699e7aa8e946499c437ad682
BaldinoMilan <104149368+BaldinoMilan@users.noreply.github.com>
2022-04-23 15:50:00 +0200

    Update Rpg_map.py

--- Rpg_map.py
+++ Rpg_map.py
@@ -1,7 +1,10 @@
 import ND_arrays as nd
+import random
 block_color = {0 : (0, 255, 0)} # il n'y a que la couleur de l'herbe, je croit que c'est vert mais modifie la si c pas ça
 # c'est un dictionnaire référence de toute les couleur pour la map(si il y a des items non défini il faut les mettres la dedans)
-Map = nd((901, 901))
-Map.zeros()
-middle = (450, 450)
+Map = {}
+for i in range(1, 9):
+  Map[f"zone{i}"] = nd.Nd_arrays((100, 100))
+  Map[f"zone{i}"][random.randint(0, 100), random.randint(0, 100)] = Png_object # <-- faut creer un objeet png ou jsp 
+
 # modifier la map avec Map[coordonées] = truc que tu veux rajouter
