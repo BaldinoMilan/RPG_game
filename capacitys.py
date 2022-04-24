fb0f2337a716ee24387edd66fb18fa19d3fb0cfd
PitiPipouSauvage <96929981+PitiPipouSauvage@users.noreply.github.com>
2022-04-25 00:53:21 +0100

    update 0.2

--- capacitys.py
+++ capacitys.py
@@ -17,44 +17,40 @@ class capacity:
         liste = [self.change_PV, self.change_PVB, self.change_PAT, self.change_PATB, self.change_PDEF]
         if True in list:
             counting = list.count(True)        
-            if counting > 1:
-                pass 
-            
-            elif counting == 1:
-                self.empty = liste.index("True")
+            self.empty = liste.index("True")
                 
-                if self.more == True:
-                    if self.empty == liste[0]:
-                        crea.target.PV += self.ammount
-                        
-                    elif self.empty == liste[1]:
-                        crea.target.PVB += self.ammount
-                        
-                    elif self.empty == liste[2]:
-                        crea.target.PAT += self.ammount 
-                        
-                    elif self.empty == liste[3]:
-                        crea.target.PATB += self.ammount
-                        
-                    elif self.empty == liste[4]:
-                        crea.target.PDEF += self.ammount
-                        
-                elif self.less == True:
-                    self.ammount = -self.ammount 
-                    if self.empty == liste[0]:
-                        crea.target.PV += self.ammount
-                        
-                    elif self.empty == liste[1]:
-                        crea.target.PVB += self.ammount
-                        
-                    elif self.empty == liste[2]:
-                        crea.target.PAT += self.ammount 
-                        
-                    elif self.empty == liste[3]:
-                        crea.target.PATB += self.ammount
-                        
-                    elif self.empty == liste[4]:
-                        crea.target.PDEF += self.ammount
+            if self.more == True:
+                if self.empty == liste[0]:
+                    crea.target.PV += self.ammount
+                    
+                elif self.empty == liste[1]:
+                    crea.target.PVB += self.ammount
+                    
+                elif self.empty == liste[2]:
+                    crea.target.PAT += self.ammount 
+                    
+                elif self.empty == liste[3]:
+                    crea.target.PATB += self.ammount
+                    
+                elif self.empty == liste[4]:
+                    crea.target.PDEF += self.ammount
+                    
+            elif self.less == True:
+                self.ammount = -self.ammount 
+                if self.empty == liste[0]:
+                    crea.target.PV += self.ammount
+                    
+                elif self.empty == liste[1]:
+                    crea.target.PVB += self.ammount
+                    
+                elif self.empty == liste[2]:
+                    crea.target.PAT += self.ammount 
+                    
+                elif self.empty == liste[3]:
+                    crea.target.PATB += self.ammount
+                    
+                elif self.empty == liste[4]:
+                    crea.target.PDEF += self.ammount
         
             
 os.system("pause")
