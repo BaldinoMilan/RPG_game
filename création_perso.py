c81a0e2d1a7e51c422030459828cb79a18a29ce1
PitiPipouSauvage <96929981+PitiPipouSauvage@users.noreply.github.com>
2022-05-02 22:31:58 +0100

    update 0.3.5

--- création_perso.py
+++ création_perso.py
@@ -15,7 +15,8 @@ import monster as mob
 capacitys = {}
 
 advantages = ["PV", "PDEF"]
-            
+
+food_available = []
 
 #PV = Points de vie DE BASE
 #PAT = Points d'attaque DE BASE
@@ -25,8 +26,13 @@ advantages = ["PV", "PDEF"]
 #système d'attaque incomplet car les monstres et autres types d'ennemis ne sont implémentés et que les différentes features des armes
 #notemment sur les différentes attaques dispos etc... ne sont pas encore implémentés
 
+class food:
+    def __init__(self, adv, ammount):
+        self.adv = adv
+        self.ammount = ammount 
+
 class character:
-    def __init__(self, name, PV, PAT, PDEF = 0, effect = [], inv = [], armor = None):
+    def __init__(self, name, PV, PAT, PDEF = 0, effect = [], inv = [], armor = None, attacked = False, in_fight = False):
         self.name = name
         self.PV = PV 
         self.PVB = PV 
@@ -37,6 +43,8 @@ class character:
         self.pos = (49, 49) 
         self.inv = inv 
         self.effect = effect
+        self.attacked = attacked
+        self.in_fight = in_fight
         
         if self.name == "Carlos de Miguel":
             gamerule_churros = True 
@@ -75,9 +83,7 @@ class character:
         
 # est-ce qu'on fait un système dd'inventaire aussi ?
 
-class food:
-    def __init__(self, adv, ammount):
-        self.adv = adv
-        self.ammount = ammount 
-
-food_available = []
+class paladin(character):
+    def retaliation(self):
+        if self.in_fight and self.attacked:
+            pass
